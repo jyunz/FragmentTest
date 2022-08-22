068f2d59c2057eb1aa40cb0b13ad1e6592f60197
Roy <J.jyun0525@gmail.com>
2022-08-22 17:39:05 +0900

    Adapter.kt 1,2 작성

--- app/src/main/java/com/roy/fragmenttest/adapter/FirstAdapter.kt
+++ app/src/main/java/com/roy/fragmenttest/adapter/FirstAdapter.kt
@@ -2,11 +2,33 @@ package com.roy.fragmenttest.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.roy.fragmenttest.R
 import com.roy.fragmenttest.data.Store
 
-class FirstAdapter(val mContext : Context, val resId : Int, val mList : List<Store>) : ArrayAdapter<Store>(mContext,resId,mList) {
+class FirstAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : List<Store>) : ArrayAdapter<Store>(mContext,resId,mList) {
 
     val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.fragment_first_list,null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
+        storeNameTxt.text = data.name
+
+        return  row
+    }
 
 }
--- app/src/main/java/com/roy/fragmenttest/adapter/SecondAdapter.kt
+++ app/src/main/java/com/roy/fragmenttest/adapter/SecondAdapter.kt
@@ -1,4 +1,34 @@
 package com.roy.fragmenttest.adapter
 
-class SecondAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.roy.fragmenttest.R
+import com.roy.fragmenttest.data.Store
+
+class SecondAdapter (val mContext : Context,
+val resId : Int,
+val mList : List<Store>) : ArrayAdapter<Store>(mContext,resId,mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.fragment_second_list, null)
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
+
+        storeNameTxt.text = data.name
+
+        return row
+    }
 }
