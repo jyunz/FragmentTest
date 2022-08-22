48c4b40ad93180462cdb50cdb120f1a85651aa3d
Roy <J.jyun0525@gmail.com>
2022-08-22 17:28:45 +0900

    DetailView kt와 xml작업

---
+++ app/src/main/java/com/roy/fragmenttest/ViewDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.roy.fragmenttest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewDetailActivity :BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_detail)
+    }
+
+    override fun setUpEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+}
